404c03a2e3201a86dfcce9585c177dd27821da6a
Nulikontik <nurlanbekovulanbek@gmail.com>
2023-11-10 01:12:52 +0600

    херовый js ради галочки

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Получаем ссылку на кнопку "View More" и на содержимое слайда
-  const viewMoreButton = document.querySelector(".card:nth-child(1) .button");
-  const slideContent = document.querySelector(
-    ".card:nth-child(1) .card-content"
-  );
+// Получаем элементы слайдера и слайдов
+var slider = document.querySelector(".swiper");
+var slides = document.querySelectorAll(".swiper-slide");
 
-  // Добавляем обработчик события на клик по кнопке
-  viewMoreButton.addEventListener("click", function () {
-    // Изменяем содержимое слайда (например, текст)
-    slideContent.innerHTML = `
-        <h2 class="name">David Dell</h2>
-        <p class="description">
-          This is the updated content of the first slide. You clicked "View More".
-        </p>
-        <button class="button">View More</button>
-      `;
+// Устанавливаем начальные значения
+var currentSlideIndex = 0;
+var isAnimating = false;
 
-    // Можно также выполнить другие действия или анимации по желанию
-  });
-});
+// Функция для обработки событий скролла
+function handleScroll(event) {
+  // Проверяем, идет ли анимация
+  if (isAnimating) return;
+
+  // Определяем направление скролла
+  var delta = event.deltaY;
+  if (delta > 0 && currentSlideIndex < slides.length - 1) {
+    // Прокрутка вниз
+    animateSlide(currentSlideIndex + 1);
+  } else if (delta < 0 && currentSlideIndex > 0) {
+    // Прокрутка вверх
+    animateSlide(currentSlideIndex - 1);
+  }
+}
+
+// Функция для анимации перехода между слайдами
+function animateSlide(nextIndex) {
+  isAnimating = true;
+
+  // Получаем текущий и следующий слайд
+  var currentSlide = slides[currentSlideIndex];
+  var nextSlide = slides[nextIndex];
+
+  // Увеличиваем размер следующего слайда
+  nextSlide.style.transform = "scale(1.1)";
+  nextSlide.style.opacity = "1";
+
+  // Запускаем анимацию
+  setTimeout(function () {
+    // Уменьшаем размер текущего слайда
+    currentSlide.style.transform = "scale(0.9)";
+    currentSlide.style.opacity = "0";
+
+    // Сбрасываем стили для следующего слайда
+    nextSlide.style.transform = "";
+    nextSlide.style.opacity = "";
+
+    // Обновляем текущий индекс слайда
+    currentSlideIndex = nextIndex;
+    isAnimating = false;
+  }, 600); // Время анимации в миллисекундах
+}
+
+// Добавляем обработчик событий скролла
+document.addEventListener("wheel", handleScroll);
